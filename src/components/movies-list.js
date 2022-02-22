a1460d084eab9d9cc585fcfc3850ee7a43075659
George Harland <georgeharland12@gmail.com>
2022-02-22 22:56:08 +0000

    daily progress

--- src/components/movies-list.js
+++ src/components/movies-list.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import MovieDataService from "../services/movies"
+import { Link } from "react-router-dom"
 
 function MoviesList() {
     return (
