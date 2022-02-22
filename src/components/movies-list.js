c391a6e865e91da3d74d6eefaec0eaba80a0ea5f
George Harland <georgeharland12@gmail.com>
2022-02-22 23:10:08 +0000

    quick fix

--- src/components/movies-list.js
+++ src/components/movies-list.js
@@ -2,12 +2,13 @@ import React, {useState, useEffect} from 'react'
 import MovieDataService from "../services/movies"
 import { Link } from "react-router-dom"
 
-function MoviesList() {
-    return (
-        <div className = "App">
-            Movies List
-        </div>
-    );
+const MoviesList = props => {
+
+    const [movies, setMovies] = useState([])
+    const [searchTitle, setSearchTitle] = useState("")
+    const [searchRating, setSearchRating] = useState("")
+    const [ratings, setRatings] = useState(["All Ratings"])
+
 }
 
 export default MoviesList;
