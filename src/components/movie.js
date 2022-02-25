35fe87b5fc345780ef203650017d6714e1b50e45
George Harland <georgeharland12@gmail.com>
2022-02-25 16:19:30 +0000

    Listing reviews

--- src/components/movie.js
+++ src/components/movie.js
@@ -54,8 +54,31 @@ const Movie = props => {
                                 </Link>}
                             </Card.Body>
                         </Card>
+                        <h2>Reviews</h2>
                         <br></br>
-                        <h2>Revieww</h2>
+                        {movie.reviews.map((review, index) => {
+                            return (
+                                <Media key={ index }>
+                                    <Media.Body>
+                                        <h5>{ review.name + " reviewed on " + review.date }</h5>
+                                        <p>{ review.review }</p>
+                                        { props.user && props.user.id === review.user_id &&
+                                            <Row>
+                                                <Col>
+                                                    <Link to={{
+                                                        pathname:"/movies/" +
+                                                            props.match.params.id +
+                                                            "/review",
+                                                        state: { currentReview: review }
+                                                    }}>Edit</Link>
+                                                </Col>
+                                                <Col><Button variant="link">Delete</Button></Col>
+                                            </Row>
+                                        }
+                                    </Media.Body>
+                                </Media>
+                            )
+                        })}
                     </Col>
                 </Row>
             </Container>
