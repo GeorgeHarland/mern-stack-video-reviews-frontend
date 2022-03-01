5fd6024f8939eb486026704fdfad634290151292
George Harland <georgeharland12@gmail.com>
2022-03-01 19:08:06 +0000

    can move to the next page in the movie list

--- src/components/movies-list.js
+++ src/components/movies-list.js
@@ -11,21 +11,48 @@ import Card from 'react-bootstrap/Card';
 
 const MoviesList = props => {
 
+    // State variables
     const [movies, setMovies] = useState([])
     const [searchTitle, setSearchTitle] = useState("")
     const [searchRating, setSearchRating] = useState("")
     const [ratings, setRatings] = useState(["All Ratings"])
+
+    const [currentPage, setCurrentPage] = useState(0)
+    const [entriesPerPage, setEntriesPerPage] = useState(0)
+    const [currentSearchMode, setCurrentSearchMode] = useState("")
 
     useEffect(() => {
         retrieveMovies()
         retrieveRatings()
     }, [])
 
+    useEffect(() => {
+        setCurrentPage(0)
+    }, [currentSearchMode])
+
+    // Will trigger every time the page changes
+    useEffect(() => {
+        retrieveNextPage()
+    }, [currentPage])
+
+    const retrieveNextPage = () => {
+        if(currentSearchMode === "findByTitle") {
+            findByTitle()
+        } else if(currentSearchMode === "findByRating") {
+            findByRating()
+        } else {
+            retrieveMovies()
+        }
+    }
+
     const retrieveMovies = () => {
-        MovieDataService.getAll()
+        setCurrentSearchMode("")
+        MovieDataService.getAll(currentPage)
             .then(response => {
                 console.log(response.data)
                 setMovies(response.data.movies)
+                setCurrentPage(response.data.page)
+                setEntriesPerPage(response.data.entries_per_page)
             })
             .catch( e => {
                 console.log(e)
@@ -54,19 +81,23 @@ const MoviesList = props => {
     }
 
     const find = (query, by) => {
-        MovieDataService.find(query, by)
+        MovieDataService.find(query, by, currentPage)
             .then(response => {
                 console.log(response.data)
                 setMovies(response.data.movies)
+                setCurrentPage(response.data.page)
+                setEntriesPerPage(response.data.entries_per_page)
             })
             .catch(e => {
                 console.log(e)
             })
     }
     const findByTitle = () => {
+        setCurrentSearchMode("findByTitle")
         find(searchTitle, "title")
     }
     const findByRating = () => {
+        setCurrentSearchMode("findByRating")
         if(searchRating === "All Ratings") {
             retrieveMovies()
         }
@@ -144,6 +175,14 @@ const MoviesList = props => {
                             )
                         })}
                     </Row>
+                    <br />
+                        Showing page: { currentPage + 1 }.
+                    <Button 
+                        variant="link"
+                        onClick={() => { setCurrentPage(currentPage + 1)}}
+                    >
+                        Get next { entriesPerPage } results
+                    </Button>
             </Container>
         </div>
     )
